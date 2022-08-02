b05fb67ce33340dcb7012289bab3407e66cce699
zman811 <sunburnzman@gmail.com>
2022-08-02 16:57:45 -0600

    Slow solution for 1

---
+++ 1.js
@@ -0,0 +1,48 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+const twoSum = (nums, target) => {
+  // First way i could go about this is itterate through both arrays, while this would give me a solution it would be O(n^2) time complexity
+  // Ill start with it this way then see if i can get a more effecient solution
+  // * this is the slow but working solution
+  for(let i = 0; i < nums.length; i++) {
+    for(let j = 0; j < nums.length; j++) {
+      if(nums[i] + nums[j] === target && i !== j) {
+        return [i, j]
+      }
+    }
+  }
+  // * add in another faster solution below
+};
+
+const test = twoSum([3,3], 6)
+console.log(test)
