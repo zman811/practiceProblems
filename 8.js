b58602567ee798aa4a32299fd37cfac3f2dbfc2e
zman811 <sunburnzman@gmail.com>
2022-08-09 18:12:50 -0600

    Working solution, not the best but working

--- 8.js
+++ 8.js
@@ -27,9 +27,33 @@ nums is sorted in ascending order.
  * @param {number} target
  * @return {number}
  */
-const search = function(nums, target) {
+const search = function (nums, target) {
   //make a resut var
+  let result = -1; // -1 by default if nothing is found
   // current middle
+  let start = 0;
+  let end = nums.length - 1;
+  // edge case
+  if (nums[end] === target) {
+    return end;
+  }
+  while (start < end) {
+    let middleIndex = Math.floor((end + start) / 2);
+    let middle = nums[middleIndex];
+    if (middle === target) {
+      result = middleIndex;
+      return result;
+    }
+    if (middle < target) {
+      start = middleIndex + 1;
+    } else if (middle > target) {
+      end = middleIndex;
+    }
+  }
+  return result;
   // check if less than or greater than
   // while loop/recurse moving the middle, if it is ever a impossible spot then we return -1 since it is not found
 };
+
+console.log("4, :", search([-1, 0, 3, 5, 9, 12, 13], 13));
+console.log("-1, :", search([-1, 0, 3, 5, 9, 12], 2));
