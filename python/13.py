47d6de3fb81cbfc2f682d3b9dbe901301567a0de
zman811 <sunburnzman@gmail.com>
2022-08-19 15:33:35 -0600

    Added file for python pratice

---
+++ python/13.py
@@ -0,0 +1,9 @@
+class LinkedList:
+  def hasCycle(self, head):
+    slow = fast = head
+    while fast and fast.next:
+      fast = fast.next.next
+      slow = slow.next
+      if slow == fast:
+        return True
+    return False
